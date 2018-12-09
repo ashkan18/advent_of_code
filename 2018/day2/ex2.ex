64efccfa7bab610a23c4a00689f928f77482ef85
ashkan <ashkan@artsymail.com>
2018-12-09 10:15:07 -0500

    First pass on day3

--- 2018/day2/ex2.ex
+++ 2018/day2/ex2.ex
@@ -9,8 +9,7 @@ defmodule Day2Ex2 do
 
   # Find correct box ids from from a list of box ids
   defp find_correct([]), do: IO.puts("Could not find correct boxes.")
-  defp find_correct(box_ids) do
-    [head|tail] = box_ids
+  defp find_correct([head|tail]) do
     case(find_in_list(head, tail)) do
       {:found, {box1, box2}} -> IO.puts("Correct box ids: #{box1} , #{box2}")
       _ -> find_correct(tail)
@@ -19,8 +18,7 @@ defmodule Day2Ex2 do
 
   # Find possible corrects for this box id
   defp find_in_list(_box_id, []), do: false
-  defp find_in_list(box_id, box_ids) do
-    [head|tail] = box_ids
+  defp find_in_list(box_id, [head|tail]) do
     if diff_is_one?(box_id, head), do: {:found, {box_id, head}}, else: find_in_list(box_id, tail)
   end
 

409e5d02a3e76879e1b9a4da4e3afff830b8ce1f
Ashkan Nasseri <ashkan@artsymail.com>
2019-12-11 21:57:13 -0500

    Add part 2

--- 2019/day6/part1.exs
+++ 2019/day6/part1.exs
@@ -1,9 +1,15 @@
 defmodule Part1 do
-  def run do
+  def run1 do
     read_input()
     |> generate_map()
     |> calculate_checksum()
     |> IO.inspect()
+  end
+
+  def run2 do
+    read_input()
+    |> generate_map()
+    |> find_shortest("YOU", "SAN")
   end
 
   defp read_input, do: File.open!("data") |> IO.stream(:line) |> Stream.map(&String.trim/1) |> Stream.map(&(String.split(&1, ")")))
@@ -29,6 +35,24 @@ defmodule Part1 do
     |> Map.get(key)
     |> find_orbits(map, count + 1)
   end
+
+  defp find_shortest(map, start, finish) do
+    start_path_to_com = path(start, map, []) |> Enum.reverse() |> Enum.drop(1) |> IO.inspect()
+    finish_path_to_com = path(finish, map, []) |> Enum.reverse() |> Enum.drop(1) |> IO.inspect()
+    start_path_to_com
+    |> Enum.reduce_while(0, &find_in_finish_path(&1, &2, finish_path_to_com))
+    |> IO.inspect()
+  end
+
+  defp path(nil, _map, path), do: path
+  defp path(start, map, path), do: path(Map.get(map, start), map, [start | path])
+
+  defp find_in_finish_path(node, current_count, finish_path_to_com) do
+    case Enum.find_index(finish_path_to_com, fn x -> x == node end) do
+      nil -> {:cont, current_count + 1}
+      index -> {:halt, current_count + index}
+    end
+  end
 end
 
-Part1.run()
+Part1.run2()

5ba8ba525032f98652a4f4eddaa9411729f35b52
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2021-12-15 08:25:44 -0500

    day2-partII

--- 2021/day2/solution.ex
+++ 2021/day2/solution.ex
@@ -1,7 +1,13 @@
 defmodule Day2 do
-  def run do
+  def part1 do
     read_input()
     |> Enum.reduce({0, 0}, &update_location/2)
+    |> IO.inspect()
+  end
+
+  def part2 do
+    read_input()
+    |> Enum.reduce({0, 0, 0}, &update_location2/2)
     |> IO.inspect()
   end
 
@@ -17,7 +23,14 @@ defmodule Day2 do
   defp update_location(["up", y], {horizontal, depth}), do: {horizontal, depth - y}
   defp update_location(["down", y], {horizontal, depth}), do: {horizontal, depth + y}
 
+
+  defp update_location2(["forward", x], {horizontal, depth, aim}), do: {horizontal + x, depth + (aim * x), aim}
+  defp update_location2(["up", y], {horizontal, depth, aim}), do: {horizontal, depth, aim - y}
+  defp update_location2(["down", y], {horizontal, depth, aim}), do: {horizontal, depth, aim + y}
+
   defp parse([cmd, n]), do: [cmd, String.to_integer(n)]
 end
 
-Day2.run()
+Day2.part1()
+
+Day2.part2()

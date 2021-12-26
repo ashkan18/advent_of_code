50a6776e639b1439ca7700c9002a4cea00b7f340
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2021-12-26 14:02:18 -0500

    day 9 part 2

--- 2021/day9/part2.ex
+++ 2021/day9/part2.ex
@@ -1,24 +1,57 @@
 defmodule Day9.Part2 do
   def run do
-
+    grid = read_grid()
+    grid
+    |> low_points()
+    |> Enum.map(fn {{row, col}, _} ->
+      basin({row, col}, grid)
+      |> MapSet.size()
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.product()
   end
 
-  defp check_point(index, current_line, previous_line, next_line) do
-    current_value = Enum.at(current_line, index)
-    right = Enum.at(current_line, index + 1)
-    left = index > 0 && Enum.at(current_line, index - 1)
-    below = not is_nil(next_line) and Enum.at(next_line, index)
-    top = not is_nil(previous_line) and Enum.at(previous_line, index)
-    Enum.all?([right, left, below, top], fn compare_to -> current_value < compare_to end)
+  def low_points(grid) do
+    grid
+    |> Enum.filter(fn {{i, j}, number} ->
+      left = grid[{i - 1, j}]
+      right = grid[{i + 1, j}]
+      top = grid[{i, j - 1}]
+      bottom = grid[{i, j + 1}]
+      number < left and number < right and number < top and number < bottom
+    end)
   end
 
-  defp read_input do
-    File.open!("input")
+
+  def basin(point, grid) do
+    basin(MapSet.new(), point, grid)
+  end
+
+  def basin(set, {row, col} = point, grid) do
+    if point in set or grid[point] in [9, nil] do
+      set
+    else
+      set
+      |> MapSet.put(point)
+      |> basin({row - 1, col}, grid)
+      |> basin({row + 1, col}, grid)
+      |> basin({row, col - 1}, grid)
+      |> basin({row, col + 1}, grid)
+    end
+  end
+
+  defp read_grid do
+    lines = File.open!("input")
     |> IO.stream(:line)
     |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.graphemes/1)
-    |> Stream.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+
+    for {line, row} <- Enum.with_index(lines),
+      {number, column} <- Enum.with_index(String.to_charlist(line)),
+      into: %{} do
+        {{row, column}, number - ?0}
+      end
   end
 end
 
-Day9.Part1.run() |> IO.inspect(label: :part1)
+Day9.Part2.run() |> IO.inspect(label: :part2)

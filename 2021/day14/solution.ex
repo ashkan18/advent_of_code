d70055d8c9e708884c61fe2f23eef2574fc948a7
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2022-01-03 11:46:24 -0500

    first pass

--- 2021/day14/solution.ex
+++ 2021/day14/solution.ex
@@ -1,7 +1,19 @@
 defmodule Day14 do
-  def run do
+  def part1 do
     {input, mapping} = read_input()
+    process_input(String.graphemes(input), mapping) |> IO.inspect(label: :one) |> process_input(mapping) |> IO.inspect(label: :two)
+    # 0..1
+    # |> Enum.reduce(String.graphemes(input), fn _step, current_result ->
+    #   process_input(current_result, mapping)
+    # end)
+    # |> Enum.join()
+  end
 
+  def process_input([_a | rest], _mapping) when rest == [], do: []
+  def process_input([a, b | rest], mapping) do
+    IO.inspect({a, b, mapping["#{a}#{b}"]}, label: :checking)
+    result = [a, mapping["#{a}#{b}"]]
+    result ++ process_input([b] ++ rest, mapping)
   end
 
   def read_input do
@@ -21,4 +33,4 @@ defmodule Day14 do
   end
 end
 
-Day14.run() |> IO.inspect()
+Day14.part1() |> IO.inspect()
